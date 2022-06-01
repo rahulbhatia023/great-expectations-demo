0cb1d38f524ae0dd4df6fc49b1ebf037ff998fe7
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-06-01 23:49:38 +0530

    Code Improvements

--- ge_filesystem_profiler_validate.py
+++ ge_filesystem_profiler_validate.py
@@ -5,32 +5,31 @@ from great_expectations.profile.user_configurable_profiler import (
 
 from great_expectations import DataContext
 
-if __name__ == "__main__":
-    context = DataContext()
+context = DataContext()
 
-    # --- CREATE EXPECTATIONS --- #
-    context.create_expectation_suite(
-        expectation_suite_name="expectation_suite_profiler_demo",
-        overwrite_existing=True,
-    )
+# --- CREATE EXPECTATIONS --- #
+context.create_expectation_suite(
+    expectation_suite_name="expectation_suite_profiler_demo",
+    overwrite_existing=True,
+)
 
-    validator = context.get_validator(
-        batch_request=BatchRequest(
-            datasource_name="filesystem_pandas_datasource_demo",
-            data_connector_name="filesystem_pandas_data_connector_demo",
-            data_asset_name="yellow_tripdata_sample_2019-01.csv",
-        ),
-        expectation_suite_name="expectation_suite_profiler_demo",
-    )
+validator = context.get_validator(
+    batch_request=BatchRequest(
+        datasource_name="filesystem_pandas_datasource_demo",
+        data_connector_name="filesystem_pandas_data_connector_demo",
+        data_asset_name="yellow_tripdata_sample_2019-01.csv",
+    ),
+    expectation_suite_name="expectation_suite_profiler_demo",
+)
 
-    profiler = UserConfigurableProfiler(profile_dataset=validator)
-    profiler.build_suite()
+profiler = UserConfigurableProfiler(profile_dataset=validator)
+profiler.build_suite()
 
-    validator.save_expectation_suite()
+validator.save_expectation_suite()
 
-    # --- VALIDATE DATA --- #
-    context.run_checkpoint(checkpoint_name="checkpoint_filesystem_profiler_demo")
+# --- VALIDATE DATA --- #
+context.run_checkpoint(checkpoint_name="checkpoint_filesystem_profiler_demo")
 
-    # --- CREATE DATA DOCS --- #
-    context.build_data_docs()
-    context.open_data_docs()
+# --- CREATE DATA DOCS --- #
+context.build_data_docs()
+context.open_data_docs()
--- ge_filesystem_validate.py
+++ ge_filesystem_validate.py
@@ -1,11 +1,10 @@
 from great_expectations import DataContext
 
-if __name__ == "__main__":
-    context = DataContext()
+context = DataContext()
 
-    # --- VALIDATE DATA --- #
-    context.run_checkpoint(checkpoint_name="checkpoint_filesystem_demo")
+# --- VALIDATE DATA --- #
+context.run_checkpoint(checkpoint_name="checkpoint_filesystem_demo")
 
-    # --- CREATE DATA DOCS --- #
-    context.build_data_docs()
-    context.open_data_docs()
+# --- CREATE DATA DOCS --- #
+context.build_data_docs()
+context.open_data_docs()
